a6942d12d5800e90b2c32540f175930ae4cc9818
Aaric Pittman <aaricpittman@gmail.com>
2016-11-11 09:35:19 -0600

    dashboard requires user to be signed in

---
+++ spec/features/dashboard_requires_user_to_be_signed_in_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "dashboard requires user to be signed in", type: :feature do
+  context "when not signed in" do
+    scenario "it should send them to the sign in page" do
+      visit root_path
+
+      expect(current_path).to eq(new_user_session_path)
+    end
+  end
+
+  context "when signed in" do
+    scenario "is should show their bookings", :wip do
+      user = create(:user, password: "abc123")
+      sign_in_with(user.email, "abc123")
+
+      visit root_path
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content "You currently have no bookings."
+    end
+  end
+end
