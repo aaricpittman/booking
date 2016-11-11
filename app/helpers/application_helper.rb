32045ef933c49fd603cfc42d08b149524d74e367
Aaric Pittman <aaricpittman@gmail.com>
2016-11-11 16:00:07 -0600

    add bootstrap framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def rails_to_bootstrap_alert(flash_type)
+    case flash_type
+    when 'notice'
+      'info'
+    when 'alert'
+      'warning'
+    else
+      flash_type
+    end
+  end
 end
